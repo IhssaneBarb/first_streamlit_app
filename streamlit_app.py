343d290cc54519c348e46307678406d48b2d237d
IhssaneBarb <barbouch.ihssane@gmail.com>
2022-11-21 16:41:47 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,6 @@
 import streamlit
 streamlit.title("My parents New healthy dinner")
 streamlit.title("Breakfast menu")
-streamlit("Omega 3 & Blueberry Oatmeal ")
+streamlit.text("Omega 3 & Blueberry Oatmeal ")
+streamlit.text("Kale, Spinach & Rocket smoothie ")
+streamlit.text("Hard-boiled Free-Range egg ")
